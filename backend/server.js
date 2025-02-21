d15810b8c4c048c34d3b17834c80f76ed14bb317
senb7 <bishal.sen@patancollege.edu.np>
2025-02-21 21:35:27 +0545

    updated backend server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 connectDB();
@@ -13,8 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// using url endpoints
-app.use('/api/users', authRoutes);
+
+// API ENDPOINTS
+// url endpoints for all users
+app.use('/api/users', userRoutes);
+
 
 // run the server
 const PORT = process.env.PORT || 8080;
