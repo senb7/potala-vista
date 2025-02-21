2d325f2c9e6e2433ef5a59f6dbbdde3e208e6f37
senb7 <bishal.sen@patancollege.edu.np>
2025-02-21 21:40:22 +0545

    Updated userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,11 +1,16 @@
-// userModel.js
+// models/userModel.js
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "agency", "visitor"], required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "agency", "visitor"], required: true },
+    resetCode: { type: String },
+    resetCodeExpiry: { type: Date }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
